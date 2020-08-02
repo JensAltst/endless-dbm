431910018263131f31e57026b80a4590c2383aae
Mike DeLuca <32890117+mdeluca0@users.noreply.github.com>
2020-08-02 12:00:34 -0600

    Update MaidenOfVirtue.lua

--- DBM_Karazhan/MaidenOfVirtue.lua
+++ DBM_Karazhan/MaidenOfVirtue.lua
@@ -7,7 +7,7 @@ Maiden.MinVersionToSync = 2.7
 Maiden:AddOption("HolyFireWarn", true, DBM_MOV_OPTION_2);
 Maiden:AddOption("RangeCheck", true, DBM_MOV_OPTION_1, function()
 	DBM:GetMod("Maiden").Options.RangeCheck = not DBM:GetMod("Maiden").Options.RangeCheck;
-	
+
 	if DBM:GetMod("Maiden").Options.RangeCheck and DBM:GetMod("Maiden").InCombat then
 		DBM_Gui_DistanceFrame_Show();
 	elseif not DBM:GetMod("Maiden").Options.RangeCheck and DBM:GetMod("Maiden").InCombat then
@@ -27,9 +27,9 @@ Maiden:RegisterCombat("YELL", DBM_MOV_YELL_PULL);
 
 function Maiden:OnCombatStart()
 	self:EndStatusBarTimer("Repentance");
-	self:StartStatusBarTimer(25, "Next Repentance", "Interface\\Icons\\Spell_Holy_PrayerOfHealing");
-	self:ScheduleSelf(20, "RepWarning");
-	
+	self:StartStatusBarTimer(45, "Next Repentance", "Interface\\Icons\\Spell_Holy_PrayerOfHealing");
+	self:ScheduleSelf(40, "RepWarning");
+
 	if self.Options.RangeCheck then
 		DBM_Gui_DistanceFrame_Show();
 	end
@@ -44,12 +44,12 @@ end
 function Maiden:OnEvent(event, arg1)
 	if event == "RepWarning" then
 		self:Announce(DBM_MOV_WARN_REP_SOON, 1);
-		
+
 	elseif event == "CHAT_MSG_MONSTER_YELL" then
 		if arg1 and (string.find(arg1, DBM_MOV_YELL_REP_1) or string.find(arg1, DBM_MOV_YELL_REP_2)) then
 			self:SendSync("Rep");
 		end
-		
+
 	elseif event == "SPELL_AURA_APPLIED" then
 		if arg1.spellId == 29522 then
 			self:Announce(string.format(DBM_MOV_WARN_HOLYFIRE, tostring(arg1.destName)), 2);
@@ -62,8 +62,8 @@ function Maiden:OnSync(msg)
 		self:Announce(DBM_MOV_WARN_REP, 3);
 		self:EndStatusBarTimer("Next Repentance");
 		self:UnScheduleSelf("RepWarning");
-		self:StartStatusBarTimer(25, "Next Repentance", "Interface\\Icons\\Spell_Holy_PrayerOfHealing");
+		self:StartStatusBarTimer(33, "Next Repentance", "Interface\\Icons\\Spell_Holy_PrayerOfHealing");
 		self:StartStatusBarTimer(12, "Repentance", "Interface\\Icons\\Spell_Holy_PrayerOfHealing");
-		self:ScheduleSelf(21, "RepWarning");
+		self:ScheduleSelf(29, "RepWarning");
 	end
-end
+end 
